f192deafc1b0d6f8d7f521ef9ba10b4ecdf57766
Jonas Stasseyns <stasseynsjonas@gmail.com>
2017-03-24 15:31:13 +0100

    Background movement skipped

--- js/tmp_logGenerator.js
+++ js/tmp_logGenerator.js
@@ -1,24 +1,19 @@
 
-
 var $logContainer = $('.log-container');
 
 setInterval(function(){
 
   var randomNum = Math.floor((Math.random() * 6));
   var badNum = Math.floor((Math.random() * 6));
-  var row = '<div class="row clearfix">';
+  var row = '<div class="row">';
 
-<<<<<<< HEAD
-  for(i=1;i<=5;i++){
-
-    row += '<div class="log '+((randomNum == i)?'target ':'')+((randomNum == badNum)?'bad':'')+'"></div>';
-
-=======
   var outputtedABadOne = false;
+
   for(i=1;i<=5;i++){
-    row += '<div class="log clearfix '+((randomNum == i)?'target ':'')+((randomNum == badNum && !outputtedABadOne)?'bad':'')+'"></div>';
+
+    row += '<div class="log '+((randomNum == i)?'target ':'')+((randomNum == badNum && !outputtedABadOne)?'bad':'')+'"></div>';
     outputtedABadOne = (randomNum == badNum);
->>>>>>> a922b621b72315e272a1fd8abb29300ab92704bf
+
   }
   row += "</div>";
 
