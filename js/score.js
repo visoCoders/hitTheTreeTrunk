363fb5b41667e2f2f4c9447d70faa6db78a98ee8
Niels Haegeman <haegeman@visocloud.org>
2017-03-24 13:47:33 +0100

    Basic Layout Score Board

--- js/score.js
+++ js/score.js
@@ -1,22 +1,12 @@
 $(function () {
     "use strict";
  
-    // for better performance - to avoid searching in DOM
-    var content = $('#content');
-    var input = $('#input');
-    var status = $('#status');
- 
-    // my color assigned by the server
-    var myColor = false;
-    // my name sent to the server
-    var myName = false;
- 
     // if user is running mozilla then use it's built-in WebSocket
     window.WebSocket = window.WebSocket || window.MozWebSocket;
  
     // if browser doesn't support WebSocket, just show some notification and exit
     if (!window.WebSocket) {
-        content.html($('<p>', { text: 'Sorry, but your browser doesn\'t '
+        console.log($('<p>', { text: 'Sorry, but your browser doesn\'t '
                                     + 'support WebSockets.'} ));
         input.hide();
         $('span').hide();
@@ -27,14 +17,12 @@ $(function () {
     var connection = new WebSocket('ws://localhost:1337'); //192.168.0.227
  
     connection.onopen = function () {
-        // first we want users to enter their names
-        input.removeAttr('disabled');
-        status.text('Choose name:');
+        
     };
  
     connection.onerror = function (error) {
         // just in there were some problems with conenction...
-        content.html($('<p>', { text: 'Sorry, but there\'s some problem with your '
+        console.log($('<p>', { text: 'Sorry, but there\'s some problem with your '
                                     + 'connection or the server is down.' } ));
     };
 
@@ -87,29 +75,7 @@ $(function () {
         }
     };
  
-    /**
-     * Send mesage when user presses Enter key
-     */
-    input.keydown(function(e) {
-        if (e.keyCode === 13) {
-            var msg = $(this).val();
-            if (!msg) {
-                return;
-            }
-            // send the message as an ordinary text
-            connection.send(msg);
-            $(this).val('');
-            // disable the input field to make the user wait until server
-            // sends back response
-            input.attr('disabled', 'disabled');
- 
-            // we know that the first message sent from a user their name
-            if (myName === false) {
-                myName = msg;
-            }
-        }
-    });
- 
+    
     /**
      * This method is optional. If the server wasn't able to respond to the
      * in 3 seconds then show some error message to notify the user that
@@ -126,10 +92,7 @@ $(function () {
     /**
      * Add message to the chat window
      */
-    function addMessage(author, message, color, dt) {
-        content.prepend('<p><span style="color:' + color + '">' + author + '</span> @ ' +
-             + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':'
-             + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes())
-             + ': ' + message + '</p>');
+    function addMessage(name, score, dead) { //
+        content.prepend();
     }
 });
