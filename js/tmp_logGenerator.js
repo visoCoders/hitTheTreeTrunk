536ae92410d1f423e622f37165f3001a8a894147
Simon Verstraete <verstraetes@visocloud.org>
2017-03-24 13:17:51 +0100

    Var with html div

--- js/tmp_logGenerator.js
+++ js/tmp_logGenerator.js
@@ -5,3 +5,12 @@ setInterval(function(){
 Math.floor((Math.random() * 6) + 1);
 
 }, 500);
+
+
+var row ="<div class=.\"log-container\">
+          <div class=\"trunk log1\"></div>
+          <div class=\"trunk log2\"></div>
+          <div class=\"trunk log3\"></div>
+          <div class=\"trunk log4\"></div>
+          <div class=\"trunk log5\"></div>
+          </div>";
