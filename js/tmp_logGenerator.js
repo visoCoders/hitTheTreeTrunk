820b8c17a0e76548ed09b05d76332ffbd31fde14
Jonas Stasseyns <stasseynsjonas@gmail.com>
2017-03-24 13:17:34 +0100

    random

--- js/tmp_logGenerator.js
+++ js/tmp_logGenerator.js
@@ -1,7 +1,10 @@
-$('.log-container').prepend('')
+
+var $logContainer = $('.log-container');
 
 setInterval(function(){
 
-Math.floor((Math.random() * 6) + 1);
+  Math.floor((Math.random() * 6) + 1);
+
+  $logContainer.prepend('');
 
 }, 500);
