67f9744f8340d18fbcce3a76ba4acc06bac8f89a
Niels Haegeman <haegeman@visocloud.org>
2017-06-09 12:04:51 +0200

    Score Board Layout update

--- js/score.js
+++ js/score.js
@@ -46,7 +46,7 @@ $(function () {
         // try to parse JSON message. Because we know that the server always returns
         // JSON this should work without any problem but we should make sure that
         // the massage is not chunked or otherwise damaged.
-        console.log(message);
+       console.log(message);
         try {
             var json = JSON.parse(message.data);
             var player = JSON.parse(json.data.utf8Data);
